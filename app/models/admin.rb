4c10386abcb98f4423cf800f440f03885813b4b7
rubicon44 <rub.b.c44@gmail.com>
2021-02-11 02:52:17 +0900

    delete association in admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,9 +4,6 @@ class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :grades
-  has_many :groups
-
   validates :username, presence: true, length: { in: 2..20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
